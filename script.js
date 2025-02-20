e8433536346c2f772c31de0eb2f5330ad14d3ee6
OnyGooD <GooDy>
2025-02-20 13:45:12 +0100

    Add game functionality to country flag selector with hint system

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 const countrySelectEl = document.getElementById("countrySelect")
 const selectedFlagEl = document.getElementById("selectedFlag")
 
+let cards = []
+let flag = undefined
+
 fetch("countries.json")
 .then(res => res.json())
 .then(countires =>{
     let selected = countires[Math.floor(Math.random()*countires.length)]
-
+    flag = selected
     let img = `https://countryflagsapi.netlify.app/flag/${selected.code}.svg`
 
     selectedFlagEl.style.backgroundImage = `url(${img})`
     countires.forEach(country => {
         let option = document.createElement("option")
         option.value = country.code
-        option.innerText = country.country
+        option.innerHTML = country.country
         countrySelectEl.appendChild(option)
     });
+
+    for(let i = 0; i < 6; i++){
+        let card = document.createElement("div")
+        selectedFlagEl.appendChild(card)
+        cards.push(card)
+    }
+
+    cards[0].classList.add("reveal")
 })
+
+function tipp(){
+    let country_code = countrySelectEl.value
+    if(flag.code === country_code){
+        alert("Helyes!")
+        cards.forEach(card => {
+            if(!card.classList.contains("reveal")){
+                card.classList.add("reveal")
+            }
+        })
+    }else{
+        cards[revealNumber].classList.add("reveal")
+        revealNumber += 1
+    }
+    if(revealNumber == 6){
+        alert("Vesztettél! Megoldás:" + flag.country)
+    }
+}
